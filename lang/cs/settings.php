a449a28825d24b77a5ddc48f9f4906223c07bb0e
GeorgeTR1 <35677090+GeorgeTR1@users.noreply.github.com>
2025-04-03 14:03:16 -0500

    Delete lang/cs directory

--- lang/cs/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Czech language file for oauthdiscord plugin
- *
- * @author David 'ilicz' Klementa <ilicz@inmail.cz>
- */
-
-$lang['key'] = 'Aplikační UID';
-$lang['secret'] = 'Aplikační heslo';
