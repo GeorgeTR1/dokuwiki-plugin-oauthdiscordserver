bbde88e5115525b68412e80b262946285d391f1d
GeorgeTR1 <35677090+GeorgeTR1@users.noreply.github.com>
2025-04-04 04:37:58 -0500

    Update helper.php

--- helper.php
+++ helper.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * DokuWiki Plugin oauthdiscord (Helper Component)
+ * DokuWiki Plugin oauthdiscordserver (Helper Component)
  *
  * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
  * @author  Andreas Gohr <dokuwiki@cosmocode.de>
  */
-class helper_plugin_oauthdiscord extends \dokuwiki\Extension\Plugin
+class helper_plugin_oauthdiscordserver extends \dokuwiki\Extension\Plugin
 {
 
 }
